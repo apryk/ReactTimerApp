2b6737961be4e102bc7310f1f8c3bd09b674eedb
apryk <aleksandr.prykhodko@gmail.com>
2017-04-02 21:23:29 +1000

    Initial commit

---
+++ app/styles/app.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'top-bar': {
+    'backgroundColor': '#333333'
+  },
+  'top-bar ul': {
+    'backgroundColor': '#333333'
+  },
+  'top-bar menu-text': {
+    'color': 'white'
+  }
+});
